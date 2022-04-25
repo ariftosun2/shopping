3b49200226c632422158f6c7203b5061dcb7d017
ArifTosu2 <ariftosun0202@gmail.com>
2022-04-25 23:26:43 +0300

    schema fixed

--- dbm/schema.sql
+++ dbm/schema.sql
@@ -1,8 +1,11 @@
-CREATE TABLE books_item(
-    id uuid DEFAULT uuid_generate_v4 (),
-    bookskind character varying(100) NOT NULL,
-    bookname character varying(100) NOT NULL,
-    detail text NOT NULL,
+-- Table: public.books_item
+-- DROP TABLE public.books_item;
+CREATE TABLE IF NOT EXISTS public.books_item (
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    bookskind character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    bookname character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    detail text COLLATE pg_catalog."default" NOT NULL,
     created date,
     CONSTRAINT news_item_pkey PRIMARY KEY (id)
-) WITH (OIDS = FALSE);
+) TABLESPACE pg_default;
+ALTER TABLE public.books_item OWNER to postgres;
