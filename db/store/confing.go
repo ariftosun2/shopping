c1d9844cf070fecba6bf9892f0e319977976ab8b
ArifTosu2 <ariftosun0202@gmail.com>
2022-06-08 23:58:40 +0300

    table create fix

--- db/store/confing.go
+++ db/store/confing.go
@@ -29,7 +29,37 @@ func OpenConnection() *sql.DB {
 		panic(err)
 	}
 
-
+	if err = createTables(db); err != nil {
+		return nil
+	}
 
 	return db
+}
+func createTables(db *sql.DB) error {
+
+	createTables := `
+	CREATE TABLE IF NOT EXISTS public.books_item (
+		id uuid NOT NULL DEFAULT uuid_generate_v4(),
+		bookskind character varying(100) COLLATE pg_catalog."default" NOT NULL,
+		bookname  character varying(100) COLLATE pg_catalog."default" NOT NULL,
+		detail text COLLATE pg_catalog."default" NOT NULL,
+		created date,
+		CONSTRAINT books_item_pkey PRIMARY KEY (id)
+	) TABLESPACE pg_default;
+	
+	ALTER TABLE public.books_item OWNER to postgres;
+	
+	CREATE TABLE IF NOT EXISTS public.user_item (
+		id uuid NOT NULL DEFAULT uuid_generate_v4(),
+		username character varying(100) COLLATE pg_catalog."default" NOT NULL,
+		lastname character varying(100) COLLATE pg_catalog."default" NOT NULL,
+		userpassword character varying(100) COLLATE pg_catalog."default" NOT NULL,
+		created date,
+		CONSTRAINT user_item_pkey PRIMARY KEY (id)
+	) TABLESPACE pg_default;
+	
+	ALTER TABLE public.user_item OWNER to postgres;
+`
+	_, err := db.Exec(createTables)
+	return err
 }
